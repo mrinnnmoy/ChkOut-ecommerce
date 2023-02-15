2ee72c58a138a3e8be1d73d461a989ad48ff3ce4
mrinnnmoy <mrinmoy202000@gmail.com>
2023-02-15 09:52:57 +0530

    update trendingProductsList

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../styles/Home.css';
 import { Link } from 'react-router-dom';
 import Helmet from '../components/Helmet/Helmet';
 import { Container, Row, Col } from 'reactstrap';
 import heroImg from '../assets/images/hero-img.png';
 import { motion } from 'framer-motion';
-
+import products from '../assets/data/products';
 import Services from '../services/Services';
 import ProductsList from '../components/UI/ProductsList';
 
 const Home = () => {
 
+  const [data, setData] = useState(products);
   const year = new Date().getFullYear()
+
+  useEffect(() => {
+    const filteredProducts = products.filter(
+      (item) => item.category === 'chair'
+    );
+
+    setData(filteredProducts);
+  }, []);
 
   return (
     <Helmet title={"Home"}>
@@ -53,7 +62,7 @@ const Home = () => {
                 Trending Products
               </h2>
             </Col>
-            <ProductsList />
+            <ProductsList data={data} />
           </Row>
         </Container>
       </section>
