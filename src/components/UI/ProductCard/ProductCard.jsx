b40a132caaa29b54f7f4245c5a0fd611c69e64a0
mrinnnmoy <mrinmoy202000@gmail.com>
2023-02-18 15:42:22 +0530

    build addToCart-notif

--- src/components/UI/ProductCard/ProductCard.jsx
+++ src/components/UI/ProductCard/ProductCard.jsx
@@ -4,8 +4,25 @@ import './ProductCard.css';
 import { motion } from 'framer-motion';
 import { Col } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { cartActions } from '../../../redux/slices/cartSlice';
+import { toast } from 'react-toastify';
 
-const ProductCard = ({item}) => {
+const ProductCard = ({ item }) => {
+
+    const dispatch = useDispatch();
+
+    const addToCart = () => {
+        dispatch(cartActions.addItem({
+            id: item.id,
+            productName: item.productName,
+            price: item.price,
+            image: item.imgUrl,
+        })
+        );
+        toast.success("Product added successfully")
+    };
+
     return (
         <Col lg='3' md='4' className='mb-2'>
             <div className='product__item'>
@@ -22,7 +39,9 @@ const ProductCard = ({item}) => {
                 </div>
                 <div className="product__card-bottom d-flex align-items-center justify-content-between p-2">
                     <span className="price">${item.price}</span>
-                    <motion.span whileTap={{ scale: 1.2 }}><i className="ri-add-line"></i></motion.span>
+                    <motion.span whileTap={{ scale: 1.2 }} onClick={addToCart}>
+                        <i className="ri-add-line"></i>
+                    </motion.span>
                 </div>
             </div>
         </Col>
