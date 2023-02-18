b7d78a1e4e9b2af9a7eccc5420c2f2922e061c91
mrinnnmoy <mrinmoy202000@gmail.com>
2023-02-18 17:41:27 +0530

    update header-cartIcon-function

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,6 +5,7 @@ import "./Header.css";
 import logo from "../../assets/images/eco-logo.png";
 import userIcon from "../../assets/images/user-icon.png";
 import { Container, Row } from 'reactstrap';
+import { useSelector } from "react-redux";
 
 
 const nav__links = [
@@ -25,6 +26,8 @@ const nav__links = [
 const Header = () => {
 
   const headerRef = useRef(null)
+  const totalQuantity = useSelector(state => state.cart.totalQuantity)
+
   const menuRef = useRef(null)
 
   const stickyHeaderFunc = () => {
@@ -75,7 +78,7 @@ const Header = () => {
               </span>
               <span className="cart__icon">
                 <i className="ri-shopping-bag-line"></i>
-                <span className="badge">1</span>
+                <span className="badge">{totalQuantity}</span>
               </span>
               <span>
                 <motion.img whileTap={{ scale: 1.2 }} src={userIcon} alt="user-icon" />
