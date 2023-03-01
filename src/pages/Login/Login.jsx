5c2c8664e3fbcb1f4458c1108ad5b595f82ef88c
mrinnnmoy <mrinmoy202000@gmail.com>
2023-03-01 08:48:44 +0530

    update login-form

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Helmet from '../../components/Helmet/Helmet';
 import { Container, Row, Col, Form, FormGroup } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import "./Login.css";
 
 const Login = () => {
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
   return (
     <Helmet title="Login">
       <section>
@@ -15,14 +19,14 @@ const Login = () => {
 
               <Form className='auth__form'>
                 <FormGroup className='form__group'>
-                  <input type="email" placeholder='Enter your Email' />
+                  <input type="email" placeholder='Enter your Email' value={email} onChange={e => setEmail(e.target.value)} />
                 </FormGroup>
 
                 <FormGroup className='form__group'>
-                  <input type="password" placeholder='Enter your Password' />
+                  <input type="password" placeholder='Enter your Password' value={password} onChange={e => setPassword(e.target.value)} />
                 </FormGroup>
 
-                <button className="buy__btn auth__btn">Login</button>
+                <button type='submit' className="buy__btn auth__btn">Login</button>
                 <p>Don't have an account?{" "}<Link to='/signup'>SignUp</Link></p>
               </Form>
             </Col>
